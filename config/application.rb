3fd0f0ca915b1cdb914b78ae58b88affc63c19b3
Edna <princessahe@hotmail.com>
2017-07-24 21:45:45 +0000

    checkpoint-16: seeding data

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #checkpoint 16 not sure if is added to this file 
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+     # -- all .rb files in that directory are automatically loaded.
+         config.autoload_paths << File.join(config.root, "lib")
   end
 end
