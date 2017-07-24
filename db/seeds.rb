6a12d6147d764df20c7f3b6b897ae9128e698a4c
Edna <princessahe@hotmail.com>
2017-07-24 22:07:58 +0000

    assignment-16: find_or_creaty_by

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,24 @@
      body:   RandomData.random_paragraph
    )
  end
+    # assignment-16
+    begin
+        Post.find_or_create_by(
+            title: "Unique Title",
+            body: "Unique Body"
+            )
+    rescue ActiveRecord::RecordNotUnique
+        retry
+    end
+    
+    begin
+        Comment.find_or_create_by(
+            body: "Unique Comment Body"
+            )
+    rescue ActiveRecord::RecordNotUnique
+        retry
+    end
+    
  posts = Post.all
  
  # Create Comments
