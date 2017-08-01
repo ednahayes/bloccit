0a8f98db219866fe8a0ee9af1b3bc96328787450
Edna <princessahe@hotmail.com>
2017-08-01 19:12:26 +0000

    checkpoint-24:Authetication Singning up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     
     before_save { self.email = email.downcase if email.present? }
 
-
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
  
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -13,6 +12,6 @@ class User < ApplicationRecord
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
 
- 
     has_secure_password
+    
 end
