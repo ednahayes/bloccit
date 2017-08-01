1bd548a1d25632d8e58816bb7a8aef9436a2fdeb
Edna <princessahe@hotmail.com>
2017-08-01 20:50:37 +0000

    assignment-24:Sign Up Confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
    end
    
    resources :users, only: [:new, :create]
-
+   
+      post 'users/confirm' => 'users#confirm'
+   
    get 'about' => 'welcome#about'
+
+
 
    root 'welcome#index'
   
