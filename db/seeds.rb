1157e5c389c0dd240c2f3ef71b1c96ea3a414924
Edna <princessahe@hotmail.com>
2017-08-02 18:29:45 +0000

    checkpoint-26:Rails Posts and Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,15 @@
  require 'random_data'
+
+ # Create Users
+ 5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all 
+ 
   # Create Topics
  15.times do
    Topic.create!(
@@ -6,15 +17,13 @@
      description:  RandomData.random_paragraph
    )
  end
- 
- topics = Topic.all
+  topics = Topic.all
 
 
  # Create Posts
  50.times do
- # #1
    Post.create!(
- # #2
+     user:   users.sample,
      topic:  topics.sample, 
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -23,16 +32,22 @@
  posts = Post.all
  
  # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+   email: 'princessahe@hotmail.com',
+   password: 'helloworld'
+ )
+
+ 
  puts "Seed finished"
+ puts "#{User.count} users created" 
  puts "#{Topic.count} topics created" 
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
