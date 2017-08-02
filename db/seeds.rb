8e580337c88b560a4cbfc5aa1d48cad10b2d7188
Edna <princessahe@hotmail.com>
2017-08-02 22:42:01 +0000

    checkpoint-27:authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,11 +39,21 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'princessahe@hotmail.com',
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+
 
  
  puts "Seed finished"
